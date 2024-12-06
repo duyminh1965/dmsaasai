73731ad1ed6a92b6d7666e47adc101dfbb369b3b
unknown <ducoday12345@gmail.com>
2024-12-06 11:18:32 +0700

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({  
-  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"]
-});
+export default clerkMiddleware(); // No `publicRoutes` directly here
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    "/",                     // Public route
+    "/api/webhooks/clerk",   // Public route
+    "/api/webhooks/stripe",  // Public route
+    "/(api|trpc)(.*)",       // All API and tRPC routes
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
   ],
 };
