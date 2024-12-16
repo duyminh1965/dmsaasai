da167eac837ff93fd6521c70d32f2162b6690e01
unknown <ducoday12345@gmail.com>
2024-12-16 10:44:16 +0700

    'commit2'

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -7,8 +7,10 @@ import Header from "@/components/shared/Header";
 import { getUserImages } from "@/lib/actions/image.actions";
 import { getUserById } from "@/lib/actions/user.actions";
 
-const Profile = async ({ searchParams }: SearchParamProps) => {
-  const page = Number(searchParams?.page) || 1;
+//{ searchParams }: SearchParamProps
+const Profile = async () => {
+  //const page = Number(searchParams?.page) || 1;
+  const page = 1;
   const { userId } = await auth();
 
   if (!userId) redirect("/sign-in");
